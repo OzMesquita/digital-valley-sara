2b8b26509ee6df565ee8f6c0631cf88621c4854f
nandoWill <bolodecenoura@alu.ufc.br>
2018-09-12 11:15:48 -0300

    adicionando verificação de data no Servlet AdicionarPeriodo

--- src/main/java/br/com/n2s/sara/controller/AdicionarPeriodo.java
+++ src/main/java/br/com/n2s/sara/controller/AdicionarPeriodo.java
@@ -31,8 +31,7 @@ public class AdicionarPeriodo extends HttpServlet {
 		
 		DAOPeriodo daoPeriodo = new DAOPeriodo();
 		Periodo periodo = new Periodo();
-		//Periodo periodo = (Periodo) session.getAttribute("periodo");
-		
+		Periodo ultimoP = trilha.getPeriodos().get(trilha.getPeriodos().size() - 1);
 		periodo.setDescricao(DescricaoPeriodo.valueOf(request.getParameter("descricao")));
 		periodo.setDataInicial(LocalDate.parse((String)request.getParameter("dataInicial"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 		periodo.setDataFinal(LocalDate.parse((String)request.getParameter("dataFinal"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
