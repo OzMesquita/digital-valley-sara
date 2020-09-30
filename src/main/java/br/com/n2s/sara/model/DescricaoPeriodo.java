f2743f51190b03d3fb54e6d2e6e9c559ea7ee8e3
Tathi <tathianesouza1@gmail.com>
2020-09-30 16:41:27 -0300

    Atualização na ordem  de descrição dos períodos

--- src/main/java/br/com/n2s/sara/model/DescricaoPeriodo.java
+++ src/main/java/br/com/n2s/sara/model/DescricaoPeriodo.java
@@ -2,10 +2,10 @@ package br.com.n2s.sara.model;
 
 public enum DescricaoPeriodo {
 	SUBMISSAO_MANUSCRITO("submissão de manuscrito"),
+	AVAL("aval do orientador"),
 	AVALIACAO("avaliação"),
 	SUBMISSAO_FINAL("submissão final"),
 	RESULTADO_FINAL("resultado final"),
-	AVAL("aval do orientador"),
 	RECURSO("recurso"),
 	AVALIACAO_RECURSO("Avaliação de recurso");
 	
