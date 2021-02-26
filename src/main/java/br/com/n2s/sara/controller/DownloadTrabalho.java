35f2e128a527d181f79e6a6077caab0dad8b6f87
felipelima <f3lip303@alu.ufc.br>
2021-02-26 11:34:42 -0300

    Adição de retorno para usuário, afim de deixar mais claro o erro ocorrido

--- src/main/java/br/com/n2s/sara/controller/DownloadTrabalho.java
+++ src/main/java/br/com/n2s/sara/controller/DownloadTrabalho.java
@@ -11,9 +11,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.n2s.sara.dao.DAOTrabalho;
 import br.com.n2s.sara.model.Trabalho;
+import br.com.n2s.sara.util.Constantes;
 
 /**
  * Servlet implementation class DownloadTrabalho
@@ -21,31 +23,41 @@ import br.com.n2s.sara.model.Trabalho;
 @WebServlet("/DownloadTrabalho")
 public class DownloadTrabalho extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
 		DAOTrabalho daoTrabalho = new DAOTrabalho();
 		Trabalho trabalho = daoTrabalho.getTrabalho(Integer.parseInt(request.getParameter("idTrabalho")));
+		int idTrabalho = Integer.parseInt(request.getParameter("idTrabalho"));
 		String endereco = "";
-		if (request.getParameter("opcaoDownload")!=null) {
-			if(request.getParameter("opcaoDownload").equals("inicial")) {
-				endereco = trabalho.getEnderecoInicial();				
-			}else {
+		if (request.getParameter("opcaoDownload") != null) {
+			if (request.getParameter("opcaoDownload").equals("inicial")) {
+				endereco = trabalho.getEnderecoInicial();
+			} else {
 				endereco = trabalho.getEndereco();
 			}
 		}
 		File arquivo = new File(endereco);
 		Path path = arquivo.toPath();
-        
-        String nome = arquivo.getName();
-        int tamanho = (int) arquivo.length();
 
-        response.setContentType(Files.probeContentType(path)); // tipo do conteúdo
-        response.setContentLength(tamanho);  // opcional
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + nome + "\"");
+		String nome = arquivo.getName();
+		int tamanho = (int) arquivo.length();
 
-        OutputStream output = response.getOutputStream();
-        Files.copy(path, output);
+		try {
+			response.setContentType(Files.probeContentType(path)); // tipo do conteï¿½do
+			response.setContentLength(tamanho); // opcional
+			response.setHeader("Content-Disposition", "attachment; filename=\"" + nome + "\"");
+
+			OutputStream output = response.getOutputStream();
+			Files.copy(path, output);
+		} catch (Exception e) {
+			System.out.println(e);
+			response.sendRedirect("avaliacao.jsp");
+			session.setAttribute(Constantes.getSESSION_MGS_ERROR(), "Erro durante download de arquivo!");
+		}
+
 	}
 
 }
